cfdb68bfc69a2e9388975a9cba35938a71a74b5d
callmeog01 <ogb.official1@gmail.com>
2022-02-28 09:02:20 +0400

    Return random strings

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const myArray = ['Rock', 'Paper', 'Scissors'];
+function computerPlay(array) {
+    return array[Math.floor(Math.random() * myArray.length)]
+}
+console.log(computerPlay(myArray))
+/*
+
+if player press rock
+    
+
+
+*/
