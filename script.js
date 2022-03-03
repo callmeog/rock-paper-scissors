f71e19107212a9586fb44ef8893e46d1787a6174
callmeog01 <ogb.official1@gmail.com>
2022-03-03 06:25:51 +0400

    Fixed function that declares the winner of the round

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const myArray = ['Rock', 'Paper', 'Scissors'];
 function computerPlay(array) {
     return array[Math.floor(Math.random() * array.length)]
 }
+const playerSelection = 'rock'
+const computerSelection = myArray;
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === 'rock' ) {
+        return "You Lose, Paper beats rock"
+    }else (computerSelection === '')
+        return true
 
+}
 
-console.log(computerPlay(myArray))
+console.log(playRound(playerSelection,computerSelection))
