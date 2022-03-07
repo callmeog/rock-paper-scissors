89295ab36d22a23098a2167ca19c03b34fbd3390
callmeog01 <ogb.official1@gmail.com>
2022-03-08 03:46:10 +0400

    Initialize rock,paper,scissors regex test

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ function    computerPlay()  {
 }
 
 function    playRound(playerSelection,  computerSelection)  {
-
+    
 }
 
+const   playerSelection     =   'rock';
+const   computerSelection   =   computerPlay();
+
+const   rock    =   /rock/i;
+const   Paper   =   /paper/i;
+const   Scissors    =   /scissors/i;
