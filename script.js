e84bc153243bd1854a4c8000dd05cce748ccd73f
callmeog01 <ogb.official1@gmail.com>
2022-03-08 03:40:14 +0400

    Re-initialize playRound function

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
-const computerGameSelection = ['Rock', 'Paper', 'Scissors'];
-function computerPlay() {
-    return computerGameSelection[Math.floor(Math.random() * computerGameSelection.length)];
+const   computerGameSelection   =   ['Rock',    'Paper',    'Scissors'];
+function    computerPlay()  {
+    return  computerGameSelection[Math.floor(Math.random()  *   computerGameSelection.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection === 'rock') {
-        return "You Lose, Paper beats rock" ;
-    }else (computerSelection === 'paper')
-    return true;
+function    playRound(playerSelection,  computerSelection)  {
+
 }
 
-const playerSelection = 'rock';
-const computerSelection = myArray;
-//console.log(playRound(playerSelection,computerSelection))
-
-function game() {
-    playRound(playerSelection, computerSelection)
-        {
-            for (let i = 0; i < 5; i++)  {
-                prompt('enter')
-                if(playerSelection === 'rock' ) {
-                    console.log('player has one point')
-                } else if(computerSelection === 'Rock') {
-                    console.log('computer has one point')
-                }
-            }
-        }
-    }
-
- 
-    
