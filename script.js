e3d8ead682a614a19b3073e0beb5da66d8c94026
callmeog01 <ogb.official1@gmail.com>
2022-03-09 15:48:35 +0400

    Initialize for loop to play game 5 times

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
 const   computerGameSelection   =   ['Rock',    'Paper',    'Scissors'];
+
+let computerScore    =   1;
+let playerScore     =   1;
 function    computerPlay()  {
     return  computerGameSelection[Math.floor(Math.random()  *   computerGameSelection.length)];
 }
 
 function    playRound(playerSelection,  computerSelection)  {
+
     
+    const   rock    =   /rock/i;
+    const   Paper   =   /paper/i;
+    const   Scissors    =   /scissors/i;
+
+    if(rock.test(playerSelection))  {
+        switch  (true)  {
+            case    computerSelection   ===    'Rock':
+                console.log('Draw, play again');
+                break;
+
+            case    computerSelection   ===    'paper':
+                playerScore++
+                console.log('Computer win you lose');
+                break;
+            
+            case    computerSelection   ===    'scissors':
+                computerScore++
+                console.log('Oh no, computer win you lose');
+                break;
+        }
+    }
+
+
 }
 
-const   playerSelection     =   'rock';
+const   playerSelection     =   prompt('Player turn');
 const   computerSelection   =   computerPlay();
+console.log(playRound(playerSelection,  computerSelection));
 
-const   rock    =   /rock/i;
-const   Paper   =   /paper/i;
-const   Scissors    =   /scissors/i;
+function    game()  {
+    playRound();
+    for(let i = 0; i < 5; i)    {
+        if(playerSelection > computerGameSelection) {
+            console.log('Congratulations you won')
+        }else if(computerSelection < playerSelection) {
+            console.log('Sorry you Lose, computer won')
+        }
+    }
+}
