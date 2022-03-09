657ce92770b401836592a25c18d7cc0d17192884
callmeog01 <ogb.official1@gmail.com>
2022-03-09 16:31:02 +0400

    If playerSelection equal to paper console.log('Its a draw')

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
 const   computerGameSelection   =   ['Rock',    'Paper',    'Scissors'];
-
-let computerScore    =   1;
-let playerScore     =   1;
 function    computerPlay()  {
     return  computerGameSelection[Math.floor(Math.random()  *   computerGameSelection.length)];
 }
+let rock = /rock/i;
+let paper = /paper/i;
+let scissors = /scissors/i;
 
 function    playRound(playerSelection,  computerSelection)  {
-
-    
-    const   rock    =   /rock/i;
-    const   Paper   =   /paper/i;
-    const   Scissors    =   /scissors/i;
-
-    if(rock.test(playerSelection))  {
-        switch  (true)  {
-            case    computerSelection   ===    'Rock':
-                console.log('Draw, play again');
-                break;
-
-            case    computerSelection   ===    'paper':
-                playerScore++
-                console.log('Computer win you lose');
-                break;
-            
-            case    computerSelection   ===    'scissors':
-                computerScore++
-                console.log('Oh no, computer win you lose');
-                break;
+    if(rock.test(playerSelection)) {
+        switch(true) {
+            case playerSelection == 'rock':
+                console.log("You Lose! Paper beats Rock");
         }
     }
-
 
 }
 
@@ -39,12 +21,12 @@ const   computerSelection   =   computerPlay();
 console.log(playRound(playerSelection,  computerSelection));
 
 function    game()  {
-    playRound();
+   /* playRound(playerSelection,  computerSelection);
     for(let i = 0; i < 5; i)    {
         if(playerSelection > computerGameSelection) {
             console.log('Congratulations you won')
         }else if(computerSelection < playerSelection) {
             console.log('Sorry you Lose, computer won')
         }
-    }
+    }*/
 }
