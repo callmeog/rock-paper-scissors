ad75ca2d9d6761c5e67ebc12a51a0c0e6be095aa
callmeog01 <ogb.official1@gmail.com>
2022-03-10 05:37:33 +0400

    Add playerSelection play

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const   computerGameSelection   =   ['Rock',    'Paper',    'Scissors'];
 function    computerPlay()  {
     return  computerGameSelection[Math.floor(Math.random()  *   computerGameSelection.length)];
 }
-let rock = /rock/i;
+/*let rock = /rock/i;
 let paper = /paper/i;
-let scissors = /scissors/i;
+let scissors = /scissors/i;*/
 
 function    playRound(playerSelection,  computerSelection)  {
-    if(rock.test(playerSelection)) {
-        switch(true) {
-            case playerSelection == 'rock':
-                console.log("You Lose! Paper beats Rock");
-        }
+    if(playerSelection  === 'rock') {
+        console.log("You Lose! Paper beats Rock")
+    }else if(playerSelection === 'paper') {
+        console.log('It\'s a draw, nobody wins')
     }
 
 }
@@ -21,12 +20,8 @@ const   computerSelection   =   computerPlay();
 console.log(playRound(playerSelection,  computerSelection));
 
 function    game()  {
-   /* playRound(playerSelection,  computerSelection);
+    playRound(playerSelection,  computerSelection);
     for(let i = 0; i < 5; i)    {
-        if(playerSelection > computerGameSelection) {
-            console.log('Congratulations you won')
-        }else if(computerSelection < playerSelection) {
-            console.log('Sorry you Lose, computer won')
-        }
-    }*/
+        
+    }
 }
