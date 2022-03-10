0c2ff14a14a8f6932fc245f716514f05f14df72a
callmeog01 <ogb.official1@gmail.com>
2022-03-10 06:48:26 +0400

    Initialize player value and computer value

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 const   computerGameSelection   =   ['Rock',    'Paper',    'Scissors'];
+let playerScore = 1;
+let computerScore = 1;
 function    computerPlay()  {
     return  computerGameSelection[Math.floor(Math.random()  *   computerGameSelection.length)];
 }
-/*let rock = /rock/i;
-let paper = /paper/i;
-let scissors = /scissors/i;*/
+
+
 
 function    playRound(playerSelection,  computerSelection)  {
     if(playerSelection  === 'rock') {
-        console.log("You Lose! Paper beats Rock")
+        console.log("You won, rock beats scissors");
+        playerScore++
     }else if(playerSelection === 'paper') {
+        console.log('It\'s a draw, nobody wins');
+    }else if(playerSelection === 'scissors') {
+        console.log('You lose computer won');
+        computerScore++
+    }else if (computerSelection === 'paper') {
         console.log('It\'s a draw, nobody wins')
+    }else if(computerSelection === 'rock') {
+        console.log('Computer lose, rock beats scissors')
+        playerScore++
+    }else if(computerSelection === 'scissors') {
+        computerScore
+        console.log('Computer won, you lose')
+    }else {
+        ''
     }
 
 }
 
 const   playerSelection     =   prompt('Player turn');
 const   computerSelection   =   computerPlay();
-console.log(playRound(playerSelection,  computerSelection));
 
 function    game()  {
-    playRound(playerSelection,  computerSelection);
-    for(let i = 0; i < 5; i)    {
-        
+    for(let i = 0; i < 5; i++)    {
+        playRound();
+    }
+    if(playerScore > computerScore) {
+        alert('Congrats, you won the game')
+    }else if(playerScore < computerScore) {
+        alert('Sorry computer won')
+    }else {
+        alert('Its a draw')
     }
 }
+game()
